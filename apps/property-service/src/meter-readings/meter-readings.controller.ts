c80fd53daa938f2d1c97c8fa80a9403c7b9eb5be
Le-Thi-Truc-Ha <hale071204@gmail.com>
2026-08-11 04:28:46 +0700

    Complete database and build redis container

--- apps/property-service/src/meter-readings/meter-readings.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { MeterReadingsService } from './meter-readings.service';
-
-@Controller()
-export class MeterReadingsController {
-  constructor(private readonly service: MeterReadingsService) {}
-}
